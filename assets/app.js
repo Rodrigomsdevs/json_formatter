86ff4e2eb3c5b1acf24891ef7bfceeeccd7823ff
Rodrigo MS <rodrigoingazao@gmail.com>
2023-05-02 16:34:53 -0300

    alterado app.js para classe

--- assets/app.js
+++ assets/app.js
@@ -1,42 +1,57 @@
-function adjustTextareaHeight() {
-  const headerHeight = document.querySelector('h1').offsetHeight;
-  const textarea = document.getElementById('json-input');
-  textarea.style.height = `calc(100vh - ${headerHeight + 60}px)`;
-}
+class app_class {
 
-window.addEventListener('resize', adjustTextareaHeight);
-window.addEventListener('DOMContentLoaded', adjustTextareaHeight);
-
-document.getElementById('json-input').addEventListener('keyup', () => {
-  formatJSON();
-});
-
-formatJSON();
-
-function formatJSON() {
-  const input = document.getElementById('json-input').value;
-  const output = document.getElementById('json-output');
-
-  try {
-    const json = JSON.parse(input);
-    const jsonString = JSON.stringify(json, null, 2);
-    output.innerHTML = Prism.highlight(jsonString, Prism.languages.json, 'json');
-  } catch (error) {
-    output.textContent = 'JSON inválido';
-    console.error(`Ocorreu um erro: ${error.message}`);
+  constructor() {
+    this.input = document.getElementById('json-input');
+    this.output = document.getElementById('json-output');
+    this.themeIcon = document.getElementById('toggle-theme-icon');
+    this.toggleTheme = document.getElementById('toggle-theme');
+    this.init();
+    this.keyUpFunction();
+    this.alterThemeFunc();
   }
-}
 
-document.getElementById('toggle-theme').addEventListener('click', () => {
-  const body = document.body;
-  const themeIcon = document.getElementById('toggle-theme-icon');
-
-  if (body.classList.contains('dark-theme')) {
-    body.classList.remove('dark-theme');
-    themeIcon.className = 'icon-moon';
-  } else {
-    body.classList.add('dark-theme');
-    themeIcon.className = 'icon-sun';
+  init = () => {
+    window.addEventListener('resize', this.adjustTextareaHeight);
+    window.addEventListener('DOMContentLoaded', this.adjustTextareaHeight);
+    this.formatJSON();
   }
-  formatJSON();
-});
+
+  adjustTextareaHeight = () => {
+    const headerHeight = document.querySelector('h1').offsetHeight;
+    this.input.style.height = `calc(100vh - ${headerHeight + 60}px)`;
+  }
+
+  keyUpFunction = () => {
+    let self = this;
+    this.input.addEventListener('keyup', () => {
+      self.formatJSON();
+    });
+  }
+
+  formatJSON = () => {
+    try {
+      const json = JSON.parse(this.input.innerHTML);
+      const jsonString = JSON.stringify(json, null, 2);
+      this.output.innerHTML = Prism.highlight(jsonString, Prism.languages.json, 'json');
+    } catch (error) {
+      this.output.textContent = 'JSON inválido';
+    }
+  }
+
+  alterThemeFunc = () => {
+    this.toggleTheme.addEventListener('click', () => {
+      const body = document.body;
+      if (body.classList.contains('dark-theme')) {
+        body.classList.remove('dark-theme');
+        this.themeIcon.className = 'icon-moon';
+      } else {
+        body.classList.add('dark-theme');
+        this.themeIcon.className = 'icon-sun';
+      }
+      this.formatJSON();
+    });
+  }
+
+
+}
+let app = new app_class();
